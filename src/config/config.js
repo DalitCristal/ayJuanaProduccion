63bbc68416aa82db6c540ebc7298a344a338d354
ayJuana <dalacristal25@gmail.com>
2024-01-31 22:51:48 -0300

    cors

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 export const PORT_BACK = 8080;
 export const PORT_FRONT = 5173;
-export const HOST = "http://localhost:";
+export const HOST_BACK = "https://ayjuana.onrender.com";
+export const HOST_FRONT = "http://localhost:5173";
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import MongoStore from "connect-mongo";
 import { addLogger, logger } from "./config/logger.js";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUiExpress from "swagger-ui-express";
-import { PORT_BACK, PORT_FRONT, HOST } from "./config/config.js";
+import { PORT_BACK, HOST_FRONT } from "./config/config.js";
 
 const swaggerOptions = {
   definition: {
@@ -29,7 +29,7 @@ const swaggerOptions = {
 
 const specs = swaggerJsdoc(swaggerOptions);
 
-const whiteList = [`${HOST}${PORT_FRONT}`];
+const whiteList = [`${HOST_FRONT}`];
 
 const corsOptions = {
   origin: function (origin, callback) {
