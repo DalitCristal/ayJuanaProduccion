deb65bc4256bdd7a1ec841ece1d6f48e8742be87
ay juana front prod <dalacristal25@gmail.com>
2024-02-25 14:47:39 -0300

    cors

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const PORT_BACK = 8080;
 export const HOST_BACK = "https://ayjuana.onrender.com";
-export const HOST_FRONT = "https://ayjuana.netlify.app";
+export const HOST_FRONT = "https://demo-ayjuana.onrender.com";
